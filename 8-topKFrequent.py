6887cecb825ce5e3e964b862d282c48a0060a347
tammerrh <tammerrh@gmail.com>
2023-12-20 23:26:04 -0500

    topk

--- 8-topKFrequent.py
+++ 8-topKFrequent.py
@@ -1,13 +1,15 @@
 # Given an integer array nums and an integer k, 
 # return the k most frequent elements
 
+from collections import Counter
+
 class Solution(object):
     def topKFrequent(self, nums, k):
-        a = map(nums.count, nums)
-        return a[:k]
+        count = Counter(nums)
+        return [key for key, _ in count.most_common(k)]
     
 
 mom = [1, 1, 2, 3, 4, 2]
 sol = Solution()
 
-print(sol.topKFrequent(mom, 1))
+print(sol.topKFrequent(mom, 2))
