4a146dfc716ca964d59f2e3fba8d96b4c77ab374
tammerrh <tammerrh@gmail.com>
2023-12-23 09:12:18 -0500

    anotha

---
+++ 10-smallestNotInA.py
@@ -0,0 +1,19 @@
+# given an array A of N integers
+# returns the smallest positive integer (greater than 0) that does not occur in A
+
+def mySolution(A):
+    s = sorted(A)
+    i = 1
+    while(i in A):
+        i += 1
+
+    return i
+
+def Solution(A):
+    max_val = max(A)
+    if max_val < 0:
+        return 1
+    else:
+        all_nums = set(range(1, max_val + 2))
+        missing = min(all_nums - set(A))
+        return missing
--- 8-topKFrequent.py
+++ 8-topKFrequent.py
@@ -3,13 +3,36 @@
 
 from collections import Counter
 
-class Solution(object):
+class mySolution(object):
     def topKFrequent(self, nums, k):
+        # uses Counter to get a frequency dist
         count = Counter(nums)
+        # loops through and returns the k most common
         return [key for key, _ in count.most_common(k)]
     
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        hs = {}
+        frq = {}
+        
+        for i in xrange(0, len(nums)):
+            if nums[i] not in hs:
+                hs[nums[i]] = 1
+            else:
+                hs[nums[i]] += 1
 
-mom = [1, 1, 2, 3, 4, 2]
-sol = Solution()
+        for z,v in hs.iteritems():
+            if v not in frq:
+                frq[v] = [z]
+            else:
+                frq[v].append(z)
+        
+        arr = []
+        
+        for x in xrange(len(nums), 0, -1):
+            if x in frq:
+                
+                for i in frq[x]:
+                    arr.append(i)
 
-print(sol.topKFrequent(mom, 2))
+        return [arr[x] for x in xrange(0, k)]
